eeb617396d7f9a775460fb27fa032542661e9db8
Hardik Hajela <hardik.hajela@icloud.com>
2022-12-23 22:23:39 -0500

    Update test1.java

--- main/test1.java
+++ main/test1.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 public class test1{
     public static void main(String[] args) {
+        System.out.println("Kindly Enter a the dimentions of the grid:");
+        Scanner sc = new Scanner(System.in);
         
+
+        sc.close();
     }
-}
-    
+} 
