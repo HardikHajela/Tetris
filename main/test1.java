a9e34eacff781fe90a89ca4cca98e723c6904d48
Hardik Hajela <hardik.hajela@icloud.com>
2022-12-23 23:38:14 -0500

    no err

--- main/test1.java
+++ main/test1.java
@@ -1,13 +1,27 @@
 package main;
 
 import java.util.Scanner;
+import java.util.Optional;
+import java.io.*;
 
 public class test1{
     public static void main(String[] args) {
-        System.out.println("Kindly Enter a the dimentions of the grid:");
+        //Asking the user to input the demension of Grids
+        //has to be positive 
         Scanner sc = new Scanner(System.in);
-        
-
+        int x,y;
+        while(true){
+            try{
+                System.out.println("Kindly Enter the dimentions of the grid(positive and numeric):");
+                x = sc.nextInt();
+                y = sc.nextInt();
+                break;
+            }
+            catch(Exception e){
+                //System.out.println("Kindly Enter a the dimentions of the grid:");
+                sc.nextLine();
+            }
+        }
         sc.close();
     }
 } 
